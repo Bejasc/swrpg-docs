3e5209a810b98a47a15ad9f45ae66d1790c422f4
Ben <benjamin.schulz94@gmail.com>
2021-01-04 23:09:50 +1030

    gh-pages

---
+++ precache-manifest.78ab9c62c02412fcf956be48d631ec97.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c827c6c060b776ee0ba",
+    "url": "css/app.61b58d58.css"
+  },
+  {
+    "revision": "c31e84ccfa22dc7e5a44",
+    "url": "css/chunk-daf20554.65667e26.css"
+  },
+  {
+    "revision": "95b1c3961cfed17de38f",
+    "url": "css/chunk-vendors.9afa5340.css"
+  },
+  {
+    "revision": "0f0124984e00c9dc6bda96c910af1424",
+    "url": "img/falcon.0f012498.jpg"
+  },
+  {
+    "revision": "ae41acc18241de44771335846408023e",
+    "url": "img/tatooine.ae41acc1.jpg"
+  },
+  {
+    "revision": "48e637cd3a2426ed5848b4e70443cf45",
+    "url": "index.html"
+  },
+  {
+    "revision": "0c827c6c060b776ee0ba",
+    "url": "js/app.c2c5cfd6.js"
+  },
+  {
+    "revision": "c31e84ccfa22dc7e5a44",
+    "url": "js/chunk-daf20554.7e482967.js"
+  },
+  {
+    "revision": "95b1c3961cfed17de38f",
+    "url": "js/chunk-vendors.f1673125.js"
+  },
+  {
+    "revision": "42b36b115289bc727fee99d71a73b90b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "aafb9413fea6bcafad2f434e5abe4033",
+    "url": "server-badge-48x.png"
+  }
+]);
