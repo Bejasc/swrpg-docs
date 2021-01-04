2656d2799ceb8f432dbb2ba7bd333892910bed0b
Ben <benjamin.schulz94@gmail.com>
2021-01-04 22:36:14 +1030

    Page Title now changes with nav, updated favicon

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
 import "./registerServiceWorker";
+
+import App from "./App.vue";
+import Vue from "vue";
+import router from "./router";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
@@ -11,3 +12,11 @@ new Vue({
 	vuetify,
 	render: h => h(App),
 }).$mount("#app");
+
+router.afterEach((to,from) => {
+	// Use next tick to handle router history correctly
+    // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+    Vue.nextTick(() => {
+        document.title = to.name|| "SWRPG";
+    });
+});
